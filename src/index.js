e1bfae9d8a874c5b8ba5e1a737082442331caa4d
euss99 <euss99@hotmail.com>
2023-01-23 13:13:11 -0600

    Agrengado observables, observadores y subscripciones

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
-let style = {
-    dark_mode: false,
+/* ====== Imports ====== */
+import { fromEvent } from "rxjs";
+
+/* ====== Variables ====== */
+let letterIndex = 0;
+let letterRowIndex = 0;
+const theme_toggler = document.querySelector('#theme_toggler');
+const letterRows = document.getElementsByClassName("letter-row");
+
+/* ====== Observables ====== */
+const onKeyDown$ = fromEvent(document, "keydown");
+
+/* ====== Observers ====== */
+const insertLetter = {
+    next: (event) => {
+        const pressedKey = event.key.toUpperCase();
+
+        // Condicional para que solo se tomen las letras
+        if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
+            let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+            letterBox.textContent = pressedKey;
+            letterBox.classList.add("filled-letter");
+
+            letterIndex++;
+        } 
+    }
+};
+
+const deleteLetter = {
+    next: (event) => {
+        const pressedKey = event.key.toUpperCase();
+
+        if (pressedKey === "BACKSPACE") {
+            let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+            letterBox.textContent = "";
+            letterBox.classList.remove("filled-letter");
+            letterIndex--;
+        }
+    }
 }
 
-let theme_toggler = document.querySelector('#theme_toggler');
+/* ====== Subscriptions ====== */
+onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(deleteLetter);
 
-theme_toggler.addEventListener("click", function(){ 
+/* ====== Events ====== */
+theme_toggler.addEventListener("click", darkMode);
+
+/* ====== Funcstions ====== */
+function darkMode() {
     document.body.classList.toggle("dark_mode");
 
-    if (theme_toggler.innerText == "Dark mode") {
-        theme_toggler.innerText = "Ligth mode";
-    } else {
-        theme_toggler.innerText = "Dark mode";
-    }
-});
+    theme_toggler.innerText == "Dark mode"
+        ? theme_toggler.innerText = "Ligth mode"
+        : theme_toggler.innerText = "Dark mode";
+}
